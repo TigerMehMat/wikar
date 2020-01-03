b61ccbbb7f8e3e8f32a1abeec850b6ccf1137f7f
sTiger <vova9325053@mail.ru>
2020-01-03 23:39:21 +0300

    Фикс поиска карт

--- Models/MapsModel.js
+++ Models/MapsModel.js
@@ -14,7 +14,7 @@ class MapsModel extends MainModel {
         searchMap(mapName, releaseOnly = false) {
                 let releaseRule = releaseOnly ? ' AND `release` <= NOW()' : '';
                 return new Promise((resolve, reject) => {
-                        this.query('SELECT id,name,url,reaction,aliases FROM ark_maps WHERE name LIKE ? OR name LIKE ? OR aliases LIKE ? OR aliases LIKE ?' + releaseRule, [mapName + '%', ',' + mapName + '%',mapName + '%', ',' + mapName + '%'])
+                        this.query('SELECT id,name,url,reaction,aliases FROM ark_maps WHERE name LIKE ? OR aliases LIKE ? OR aliases LIKE ?' + releaseRule, [mapName + '%',mapName + '%', '%,' + mapName + '%'])
                                 .then(resolve)
                                 .catch(reject);
                 });
