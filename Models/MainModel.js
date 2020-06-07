cd1ed58b28982c1994690bb46098497bfbc30d1f
sTiger <vova9325053@mail.ru>
2020-06-07 12:23:03 +0300

    Начало создания новых апдейтероы

--- Models/MainModel.js
+++ Models/MainModel.js
@@ -13,27 +13,6 @@ class MainModel {
          * @return {Promise<Array>}
          */
         async query(queryString, values = []) {
-                // return new Promise((resolve, reject) => {
-                //         let pool = new Pool(config.postgresql);
-                //         let result;
-                //         pool.connect()
-                //                 .then((client) => {
-                //                         console.log(1);
-                //                         client.query("SET search_path TO 'test';")
-                //                                 .then(() => {
-                //                                         console.log(2);
-                //                                         return client.query(queryString, values);
-                //                                 })
-                //                                 .then(async (res) => {
-                //                                         console.log(3);
-                //                                         result = res;
-                //                                         client.release();
-                //                                         resolve(res.rows);
-                //                                 })
-                //                                 .catch(reject)
-                //                 })
-                //                 .catch(reject);
-                // });
 
                 const client = await pool.connect();
                 let res;
@@ -41,8 +20,6 @@ class MainModel {
                         await client.query("SET search_path TO 'test';");
                         res = await client.query(queryString, values);
                 } finally {
-                        // Make sure to release the client before any error handling,
-                        // just in case the error handling itself throws an error.
                         client.release();
                 }
                 return res;
